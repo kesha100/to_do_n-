251b8623b199b9264df65b220ba7f956fc44ca37
Aruuke <arukeshxo@gmail.com>
2024-06-06 19:28:48 +0500

    to do with 3 levels

--- app/components/TaskList/index.jsx
+++ app/components/TaskList/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import TaskItem from '../TaskItem';
 
-const TaskList = () => {
-  // Render TaskItems using TaskItem component
-  // Filter tasks by status here
+const TaskList = ({ tasks, onToggleTask, onDeleteTask }) => {
   return (
-    <></>
+    <ul>
+      {tasks.map(task => (
+        <TaskItem 
+          key={task.id} 
+          task={task} 
+          onToggleTask={onToggleTask} 
+          onDeleteTask={onDeleteTask} 
+        />
+      ))}
+    </ul>
   );
 };
 
